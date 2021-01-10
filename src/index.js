368810fbb9667739e7358974b0a3b9b2b002d2f9
mpoosang <mpoosang@gmail.com>
2021-01-10 15:59:34 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus, faChevronLeft, faTrashAlt, faCheckDouble
+} from '@fortawesome/free-solid-svg-icons'
+import App from './App';
+import './index.css';
+
+library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>, 
+  document.getElementById('root')
+);
